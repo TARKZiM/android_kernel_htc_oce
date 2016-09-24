208dc11f2413c2b574c0991ae715f1529eda063f
Sultanxda <sultanxda@gmail.com>
2016-09-24 13:46:09 -0700

    usb: dwc3-msm: Remove indefinite wakeup
    Something is wrong with this wakeup; it isn't always relaxed, thus
    wreaking havoc on battery life. We already have a wakeup in smbcharger
    that is triggered on USB connection, so instead of fixing this one, just
    remove it since it's redundant.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: danny aeng <noreference@web.de>
    Change-Id: I67c53d89068784a91e1c1c8282ec7e6e616c3e1b

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2229,10 +2229,6 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 		dev_dbg(mdwc->dev, "defer suspend with %d(msecs)\n",
 					mdwc->lpm_to_suspend_delay);
 		pm_wakeup_event(mdwc->dev, mdwc->lpm_to_suspend_delay);
-#ifndef CONFIG_MACH_MSM8996_15801
-	} else {
-		pm_relax(mdwc->dev);
-#endif
 	}
 
 	atomic_set(&dwc->in_lpm, 1);
@@ -2272,9 +2268,6 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		return 0;
 	}
 
-#ifndef CONFIG_MACH_MSM8996_15801
-	pm_stay_awake(mdwc->dev);
-#endif
 
 	/* Vote for TCXO while waking up USB HSPHY */
 	if (!mdwc->xo_vote_for_charger) {
