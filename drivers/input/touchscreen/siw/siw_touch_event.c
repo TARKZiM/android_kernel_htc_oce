25cbc63c5d3794a36b3a2739f139326b36146f4b
TARKZiM <tom8476oo@gmail.com>
2018-10-11 02:26:48 +0800

    Revert "driver: siw: sw49407 disable second screen area"
    This reverts commit 619415a9dcb85ba07c12742adb8147ff265a8469.

--- drivers/input/touchscreen/siw/siw_touch_event.c
+++ drivers/input/touchscreen/siw/siw_touch_event.c
@@ -594,13 +594,8 @@ int siw_touch_init_input(void *ts_data)
 	set_bit(INPUT_PROP_DIRECT, input->propbit);
 	input_set_abs_params(input, ABS_MT_POSITION_X, 0,
 				caps->max_x, 0, 0);
-//#ifdef CONFIG_OCE_DISABLE_SECOND_SCREEN
-	input_set_abs_params(input, ABS_MT_POSITION_Y, 160,
-			ts->caps.max_y, 0, 0);
-//#else
-//	input_set_abs_params(input, ABS_MT_POSITION_Y, 0,
-//			ts->caps.max_y, 0, 0);
-//#endif
+	input_set_abs_params(input, ABS_MT_POSITION_Y, 0,
+				caps->max_y, 0, 0);
 	input_set_abs_params(input, ABS_MT_PRESSURE, 0,
 				caps->max_pressure, 0, 0);
 	input_set_abs_params(input, ABS_MT_WIDTH_MAJOR, 0,
