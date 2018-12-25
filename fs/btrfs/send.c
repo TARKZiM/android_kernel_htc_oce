8e3f84e072a8d826453498006293b1e8620c5674
TARKZiM <tom8476oo@gmail.com>
2018-12-25 10:00:19 +0800

    Revert "patch-3.18.129-131"
    This reverts commit d962f7f1d2522b6145bd6d0896b53214e6952c95.

--- fs/btrfs/send.c
+++ fs/btrfs/send.c
@@ -3255,8 +3255,7 @@ static void free_pending_move(struct send_ctx *sctx, struct pending_dir_move *m)
 	kfree(m);
 }
 
-static void tail_append_pending_moves(struct send_ctx *sctx,
-				      struct pending_dir_move *moves,
+static void tail_append_pending_moves(struct pending_dir_move *moves,
 				      struct list_head *stack)
 {
 	if (list_empty(&moves->list)) {
@@ -3266,10 +3265,6 @@ static void tail_append_pending_moves(struct send_ctx *sctx,
 		list_splice_init(&moves->list, &list);
 		list_add_tail(&moves->list, stack);
 		list_splice_tail(&list, stack);
-	}
-	if (!RB_EMPTY_NODE(&moves->node)) {
-		rb_erase(&moves->node, &sctx->pending_dir_moves);
-		RB_CLEAR_NODE(&moves->node);
 	}
 }
 
@@ -3285,7 +3280,7 @@ static int apply_children_dir_moves(struct send_ctx *sctx)
 		return 0;
 
 	INIT_LIST_HEAD(&stack);
-	tail_append_pending_moves(sctx, pm, &stack);
+	tail_append_pending_moves(pm, &stack);
 
 	while (!list_empty(&stack)) {
 		pm = list_first_entry(&stack, struct pending_dir_move, list);
@@ -3296,7 +3291,7 @@ static int apply_children_dir_moves(struct send_ctx *sctx)
 			goto out;
 		pm = get_pending_dir_moves(sctx, parent_ino);
 		if (pm)
-			tail_append_pending_moves(sctx, pm, &stack);
+			tail_append_pending_moves(pm, &stack);
 	}
 	return 0;
 
