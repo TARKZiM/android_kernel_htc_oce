ea8cf85228e140bd2899d0d28532f13dec258cfd
Siluxsept <ronny.horn.rh@gmail.com>
2021-02-22 07:27:40 +0100

    sched_boost: fix build error
    Change-Id: Icf3be88690444f3d39e27b101a0b00f89e07974f

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -292,8 +292,6 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= sched_boost_handler,
-		.extra1         = &zero,
-		.extra2		= &three,
 	},
 	{
 		.procname	= "sched_child_runs_first",
