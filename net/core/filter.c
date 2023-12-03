3159acceee3e3ca76c1c854ea6851ff3a3a2285a
Nguyễn Long <hoanglong.lumi404@gmail.com>
2023-12-03 18:37:04 +0700

    Revert "bpf: use skb_to_full_sk helper in bpf_skb_under_cgroup"
    This reverts commit 07de3fcceed6ece8c16fee43459932a1a9729354.

--- net/core/filter.c
+++ net/core/filter.c
@@ -2190,7 +2190,7 @@ BPF_CALL_3(bpf_skb_under_cgroup, struct sk_buff *, skb, struct bpf_map *, map,
 	struct cgroup *cgrp;
 	struct sock *sk;
 
-	sk = skb_to_full_sk(skb);
+	sk = skb->sk;
 	if (!sk || !sk_fullsock(sk))
 		return -ENOENT;
 	if (unlikely(idx >= array->map.max_entries))
